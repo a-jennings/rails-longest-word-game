7f68f7a46c093e89a986b7da5ef3e65e7240fd6d
Drew Jennings <andrew_jennings@ymail.com>
2021-08-16 15:35:08 +0100

    complete basic logic to game, page is without style

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+require 'json'
+require 'open-uri'
+
 class GamesController < ApplicationController
 
   def new
@@ -10,7 +13,28 @@ class GamesController < ApplicationController
   end
 
   def score
+    # first check if its an english word.
+    word_serialized = URI.open("https://wagon-dictionary.herokuapp.com/#{params[:word]}").read
+    dictionary = JSON.parse(word_serialized)
+    if dictionary['found']
+      # now check if word can be built from letters given.
+      word_array = params[:word].upcase.chars
+      letter_array = params[:letters].delete(' ').chars # puts params letters back into array
+      @score_message = correct_letters?(word_array, letter_array) ? 'Success you win!' : 'Word cannot be built from those letters'
+    else
+      @score_message = "Sorry, but #{params[:word]} does not seem to be a valid Enlgish word..."
+    end
   end
 
+  private
 
+  def correct_letters?(word_array, letter_array)
+    number_of_letters = letter_array.length
+    word_array.each do |char|
+      if letter_array.include?(char)
+        letter_array.delete_at(letter_array.index(char))
+      end
+    end
+    word_array.length + letter_array.length == number_of_letters
+  end
 end
