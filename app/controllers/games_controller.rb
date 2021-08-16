ed26bf6156565b79b1f9e59e8c1ee0f19107edc6
Drew Jennings <andrew_jennings@ymail.com>
2021-08-16 12:53:40 +0100

    randomized letters now come up on /new page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,14 @@
 class GamesController < ApplicationController
 
   def new
+    alphabet = ('A'..'Z').to_a
+    @letters = []
+    10.times do
+      @letters.push(alphabet.sample)
+    end
+    p @letters
+
+
   end
 
   def score
